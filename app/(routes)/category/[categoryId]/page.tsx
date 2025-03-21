2b7eff891eede8be0c9f74c3818ca0a6f2954954
Jiseok Shim <jiseokshim@mac.home>
2025-03-20 21:50:20 -0400

    initial commit

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -12,67 +12,53 @@ import MobileFilter from "./components/mobile-filter";
 
 export const revalidate = 0;
 
-interface CategoryPageProps{
-    params: {
-        categoryId: string;
-    },
-    searchParams:{
-        colorId: string;
-        sizeId: string;
-    }
+interface CategoryPageProps {
+  params: {
+    categoryId: string;
+  };
+  searchParams: {
+    colorId: string;
+    sizeId: string;
+  };
 }
 
-const CategoryId: React.FC<CategoryPageProps> = async ({
-    params,
-    searchParams
-}) =>{
-    //const { categoryId } = await Promise.resolve(params);
-    //const { colorId, sizeId } = await Promise.resolve(searchParams);
-    const { categoryId } = params;
-    const { colorId, sizeId } = searchParams;
-
-    const products = await getProducts({
-        categoryId,
-        colorId,
-        sizeId,
-    });
-    const sizes = await getSizes();
-    const colors = await getColors();
-    const category = await getCategory(categoryId);
-    
-    return(
-        <div className="bg-white">
-            <Container>
-                <Billboard
-                    data={category.billboard}/>
-                <div className="px-4 sm:px-6 lg:px-8 pb-24">
-                    <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-                        <MobileFilter sizes={sizes} colors={colors}/>
-                        <div className="hidden lg:block">
-                            <Filter
-                            valueKey="sizeId"
-                            name="Sizes"
-                            data={sizes}/>
-                            <Filter
-                            valueKey="colorId"
-                            name="Colors"
-                            data={colors}/>
-                        </div>
-                        <div className="mt-6 lg:col-span-4 lg:mt-0">
-                            {products.length === 0 && <NoResults/>}
-                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                                {products.map((item)=>(
-                                    <ProductCard
-                                    key={item.id}
-                                    data={item}/>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </Container>
+const CategoryId = async ({ params, searchParams }: CategoryPageProps) => {
+  const { categoryId } = params;
+  const { colorId, sizeId } = searchParams;
+  
+  const products = await getProducts({
+    categoryId,
+    colorId,
+    sizeId,
+  });
+  const sizes = await getSizes();
+  const colors = await getColors();
+  const category = await getCategory(categoryId);
+  
+  return (
+    <div className="bg-white">
+      <Container>
+        <Billboard data={category.billboard} />
+        <div className="px-4 sm:px-6 lg:px-8 pb-24">
+          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+            <MobileFilter sizes={sizes} colors={colors} />
+            <div className="hidden lg:block">
+              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+              <Filter valueKey="colorId" name="Colors" data={colors} />
+            </div>
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((item) => (
+                  <ProductCard key={item.id} data={item} />
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </Container>
+    </div>
+  );
+};
 
 export default CategoryId;
